fdf5bf22b08eca6d95bea87a7141f86015df7136
Steven ODell <saoii96@gmail.com>
2026-03-08 23:27:55 -0700

    Done until I add Tables
    Done for now until i decide to add the harder things like tables

--- MarkedDownParser.js
+++ MarkedDownParser.js
@@ -1,48 +1,38 @@
-const markedParser = (inputText) => {
-    let currentBlock = []
-    let fileToLines = (inputText).split("\n")
+const markedParserInputString = (inputText) => {
+    const currentBlock = [];
+    const fileToLines = (inputText).split("\n");
 
     if (fileToLines.length > 0) {
-    fileToLines.forEach(k => {
-        let parsedLine
-        if (k.startsWith("###")) {
-            parsedLine = k.replace("###", "<h3>")
-            parsedLine += "</h3>"
+        fileToLines.forEach(k => {
+            let parsedLine = k
+            if (k.startsWith("###")) {
+                parsedLine = k.replace("###", "<h3>")
+                parsedLine += "</h3>"
+            } else if (k.startsWith("##")) {
+                parsedLine = k.replace("##", "<h2>")
+                parsedLine += "</h2>"
+            } else if (k.startsWith("#")) {
+                parsedLine = k.replace("#", "<h1>")
+                parsedLine += "</h1>"
+            } else if (k.startsWith("-")) {
+                parsedLine = k.replace("-", "<li>")
+                parsedLine += "</li>"
+            } else if (k.startsWith(">")) {
+                parsedLine = k.replace(">", '"')
+                parsedLine += '"'
+                parsedLine += "<br>"
+            } else { parsedLine += "<br>" }
             currentBlock.push(parsedLine)
-        } 
-        else if (k.startsWith("##")) {
-            parsedLine = k.replace("##", "<h2>") 
-            parsedLine += "</h2>"
-            currentBlock.push(parsedLine)
-        } 
-        else if (k.startsWith("#")) {
-            parsedLine = k.replace("#", "<h1>") 
-            parsedLine += "</h1>"
-            currentBlock.push(parsedLine)
-        } 
-        else if (k.startsWith("-")) {
-            parsedLine = k.replace("-", "<li>") 
-            parsedLine += "</li>"
-            currentBlock.push(parsedLine)
-        } 
-        else if (k.startsWith(">")) {
-            parsedLine = k.replace(">", '"') 
-            parsedLine += '"'
-            currentBlock.push(parsedLine)
-        }   else {
-            currentBlock.push(parsedLine)
-        }
-        })
-    }
-    currentBlock = currentBlock.join("")
-    const currentBlockString = currentBlock.join("")
+        });
+    };
+    let currentBlockString = currentBlock.join("")
     if (currentBlockString.includes("**")) {
         currentBlockString = currentBlockString.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
-    }   
+    };
     if (currentBlockString.includes("*")) {
         currentBlockString = currentBlockString.replace(/\*(.*?)\*/g, "<i>$1</i>")
-    }
+    };
+    return currentBlockString
+};
 
-        return currentBlockString
-}
 
